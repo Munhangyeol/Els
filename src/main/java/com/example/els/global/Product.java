8d13d31235d0c7dc9bf164833102aaa0a5465fd0
msw0909 <mhg10181018@gmail.com>
2025-03-26 09:35:08 +0900

    Refactor: 패키지 구조 수정

--- src/main/java/com/example/els/global/Product.java
+++ src/main/java/com/example/els/global/Product.java
@@ -1,23 +1,34 @@
-package com.example.els;
+package com.example.els.global;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 
-
-@Data
+@Entity
 @Builder
 @AllArgsConstructor
-public class ProductDto {
+@Data
+public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name; // 상품명
 
     private String description; // 상품 설명
+
+
+    @Column(nullable = false)
     private int price; // 가격
 
     private int stock; // 재고 수량
 
     private String category; // 카테고리명
+
+
+
 }
--- src/main/java/com/example/els/global/ProductDto.java
+++ src/main/java/com/example/els/global/ProductDto.java
@@ -1,32 +1,23 @@
-package com.example.els;
+package com.example.els.global;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 
 
-@Entity
+
+@Data
 @Builder
 @AllArgsConstructor
-public class Product {
+public class ProductDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
     private String name; // 상품명
 
     private String description; // 상품 설명
-
-
-    @Column(nullable = false)
     private int price; // 가격
 
     private int stock; // 재고 수량
 
     private String category; // 카테고리명
-
-
-
 }
